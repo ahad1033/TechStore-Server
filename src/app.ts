241aee7967bc27a2b02bb3434f6e7fcd5f59a1e8
ahad1033 <a.ahad2232@gmail.com>
2025-07-01 08:27:57 +0600

    ...

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://tech-store-mu-ashen.vercel.app/",
+      "*",
+      // "https://tech-store-mu-ashen.vercel.app/",
     ],
     credentials: true,
   })
